f84d1a1a6cb062042bfb0eceaf9f5e498e3e7a22
Javier <japerez@nevada.unr.edu>
2022-08-08 21:37:14 -0700

    Translated movement updates in server to graphical output in client.

--- server/online_pong.py
+++ server/online_pong.py
@@ -10,6 +10,7 @@ class Pong():
     def __init__(self, match_state):
         self.match_state = match_state
         self.player_1, self.player_2 = Player("Player 1"), Player("Player 2")
+        self.start_pos()
 
     def update(self):
         if self.match_state == "wait":
@@ -33,10 +34,11 @@ class Pong():
         p1_size = self.player_1.get_name() + " pos: " + str(self.player_1.get_pos())
         p2_size = self.player_2.get_name() + " pos: " + str(self.player_2.get_pos())
         p_sizes = p1_size + '\n' + p2_size
-        return p_sizes
-
-    def set_players(self, players: (str, str)):
-        self.player_1.client_id, self.player_2.client_id = players
+        # temp - testing possible return values for client to process
+        p1_pos = str(self.player_1.get_pos()[0]) + ' ' + str(self.player_1.get_pos()[1])
+        p2_pos = str(self.player_2.get_pos()[0]) + ' ' + str(self.player_2.get_pos()[1])
+        player_pos = 'moves' + ' ' + p1_pos + ' ' + p2_pos
+        return player_pos
 
     def process_input(self, player_id: int, player_input: str):
         player_id = str(player_id)
@@ -51,6 +53,13 @@ class Pong():
             if player_input == "start":
                 pass
 
-    def off_bounds_handler(self, entity):
+    def set_players(self, players: (str, str)):
+        self.player_1.client_id, self.player_2.client_id = players
+
+    def start_pos(self):
+        self.player_1.shape.move_ip(BALL[0], GAME_H - PADDLE[1] / 2)
+        self.player_2.shape.move_ip(GAME_W * 2 - PADDLE[0] - BALL[0], GAME_H - PADDLE[1] / 2)
+
+    def off_bounds_handler(self, entity: Player):
         entity.shape.top = max(entity.shape.top, 0)
         entity.shape.bottom = min(entity.shape.bottom, WIN_H)
--- src/states/modes/online/online_states/online_match.py
+++ src/states/modes/online/online_states/online_match.py
@@ -3,7 +3,9 @@ from states.state import State
 from Constants import *
 from states.modes.online.online_states.online_commands import MatchCommand, up_command, down_command, ready_up
 from commands.command import ActiveOn
-
+from ecs.entities import Player
+from ecs.entity_manager import EntityManager
+from ecs.systems import draw_system
 
 class Online_Match(State):
     def __init__(self, game, online, is_private=False):
@@ -13,19 +15,26 @@ class Online_Match(State):
         self.curr_match, self.server_response = None, None
         self.data, self.start = "", False
         self.register_commands()
+        self.create_objects()
 
     def update(self, state):
         command_queue = self.ih.handle_input()
         for command, args in command_queue:
             command.execute(args[0])
+
         if not self.start:
             pass
         if self.data:
             self.server_response = self.network.send(self.data)
+            if 'moves' in self.server_response:
+                moves = self.server_response.split()[1:]
+                print("Player 1:", moves[0:2])
+                print("Player 2:", moves[2:])
+                self.player_1.set_cords(int(moves[0]), int(moves[1]))
+                self.player_2.set_cords(int(moves[2]), int(moves[3]))
 
     def render(self):
-        if self.server_response and not self.start:
-            print(self.server_response)
+        draw_system(self.game.screen, self.g_manager.all_component_instances("graphics"))
 
     def register_commands(self):
         self.press_up = MatchCommand(ActiveOn.BOTH, up_command, self)
@@ -35,11 +44,18 @@ class Online_Match(State):
         self.ih.register_command(pygame.K_s, self.press_down)
         self.ih.register_command(pygame.K_SPACE, self.press_space)
 
+    def create_objects(self):
+        self.g_manager = EntityManager()
+        self.create_players()
+
+    def create_players(self):
+        self.player_1 = Player("Player 1")
+        self.player_2 = Player("Player 2")
+        self.g_manager.register_entity(self.player_1)
+        self.g_manager.register_entity(self.player_2)
+
     def set_match(self, match_id):
         self.curr_match = match_id
-
-    def leave_match(self):
-        self.curr_match = None
 
     def enter_state(self):
         if not self.is_private:
