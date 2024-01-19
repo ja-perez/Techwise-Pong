3abf01cf628967ade0731cc72b219bb51ae9d6f5
Javier <japerez@nevada.unr.edu>
2024-01-19 12:34:11 -0500

    Asset path updates fixing errors with pyinstaller package.

--- src/themes/themes.py
+++ src/themes/themes.py
@@ -1,5 +1,11 @@
 from Constants import *
 from pygame import mixer
+from os.path import join
+from os import getcwd
+
+CWD = getcwd()
+if not CWD.endswith("src"):
+    CWD = join(CWD, "src")
 
 class Themes():
     def __init__(self):
@@ -28,60 +34,60 @@ class Themes():
 
     def classic(self):
         self.background_color = BLACK
-        self.background_music = "sounds/background/classic_bgm.wav"
-        self.bounce = "sounds/bounce/classic_bounce.wav"
-        self.score = "sounds/score/classic_score.wav"
+        self.background_music = join(CWD, "sounds", "background", "classic_bgm.wav")
+        self.bounce = join(CWD, "sounds", "bounce", "classic_bounce.wav")
+        self.score = join(CWD, "sounds", "score", "classic_score.wav")
         self.left_paddle_color = WHITE
         self.right_paddle_color = WHITE
-        self.ball_image = "themes/ball_images/classic_ball.png"
+        self.ball_image = join(CWD, "themes", "ball_images", "classic_ball.png")
         self.set_vfx_volume(0.3, 0.3)
 
     def cyberpunk(self):
-        self.background_color = "themes/backgrounds/cyberpunk.jpg"
-        self.background_music = "sounds/background/cyberpunk_bgm.wav"
-        self.bounce = "sounds/bounce/cyberpunk_bounce.wav"
-        self.score = "sounds/score/cyberpunk_score.wav"
+        self.background_color = join(CWD, "themes", "backgrounds", "cyberpunk.jpg")
+        self.background_music = join(CWD, "sounds", "background", "cyberpunk_bgm.wav")
+        self.bounce = join(CWD, "sounds", "bounce", "cyberpunk_bounce.wav")
+        self.score = join(CWD, "sounds", "score", "cyberpunk_score.wav")
         self.left_paddle_color = CYBERPUNK_1
         self.right_paddle_color = CYBERPUNK_2
-        self.ball_image = "themes/ball_images/cyberpunk_ball.png"
+        self.ball_image = join(CWD, "themes", "ball_images", "cyberpunk_ball.png")
         self.set_vfx_volume(0.7, 0.7)
 
     def disco(self):
-        self.background_color = "themes/backgrounds/disco2.jpg"
-        self.background_music = "sounds/background/disco_bgm.wav"
-        self.bounce = "sounds/bounce/disco_bounce.wav"
-        self.score = "sounds/score/classic_score.wav"
+        self.background_color = join(CWD, "themes", "backgrounds", "disco2.jpg")
+        self.background_music = join(CWD, "sounds", "background", "disco_bgm.wav")
+        self.bounce = join(CWD, "sounds", "bounce", "disco_bounce.wav")
+        self.score = join(CWD, "sounds", "score", "classic_score.wav")
         self.left_paddle_color = DISCO_1
         self.right_paddle_color = DISCO_2
-        self.ball_image = "themes/ball_images/disco_ball.png"
+        self.ball_image = join(CWD, "themes", "ball_images", "disco_ball.png")
         self.set_vfx_volume(0.3, 0.3)
 
     def science(self):
-        self.background_color = "themes/backgrounds/science.jpg"
-        self.background_music = "sounds/background/science_bgm.wav"
-        self.bounce = "sounds/bounce/science_bounce.wav"
-        self.score = "sounds/score/science_score.wav"
+        self.background_color = join(CWD, "themes", "backgrounds", "science.jpg")
+        self.background_music = join(CWD, "sounds", "background", "science_bgm.wav")
+        self.bounce = join(CWD, "sounds", "bounce", "science_bounce.wav")
+        self.score = join(CWD, "sounds", "score", "science_score.wav")
         self.left_paddle_color = SCIENCE_1
         self.right_paddle_color = SCIENCE_2
-        self.ball_image = "themes/ball_images/science_ball.png"
+        self.ball_image = join(CWD, "themes", "ball_images", "science_ball.png")
         self.set_vfx_volume(0.3, 0.3)
 
     def snow(self):
-        self.background_color = "themes/backgrounds/snow.jpg"
-        self.background_music = "sounds/background/snow_bgm.wav"
-        self.bounce = "sounds/bounce/snow_bounce.wav"
-        self.score = "sounds/score/snow_score.wav"
+        self.background_color = join(CWD, "themes", "backgrounds", "snow.jpg")
+        self.background_music = join(CWD, "sounds", "background", "snow_bgm.wav")
+        self.bounce = join(CWD, "sounds", "bounce", "snow_bounce.wav")
+        self.score = join(CWD, "sounds", "score", "snow_score.wav")
         self.left_paddle_color = SNOW_1
         self.right_paddle_color = SNOW_2
-        self.ball_image = "themes/ball_images/snow_ball.png"
+        self.ball_image = join(CWD, "themes", "ball_images", "snow_ball.png")
         self.set_vfx_volume(0.3, 0.3)
 
     def western(self):
-        self.background_color = "themes/backgrounds/western.jpg"
-        self.background_music = "sounds/background/western_bgm.wav"
-        self.bounce = "sounds/bounce/western_bounce.wav"
-        self.score = "sounds/score/western_score.wav"
+        self.background_color = join(CWD, "themes", "backgrounds", "western.jpg")
+        self.background_music = join(CWD, "sounds", "background", "western_bgm.wav")
+        self.bounce = join(CWD, "sounds", "bounce", "western_bounce.wav")
+        self.score = join(CWD, "sounds", "score", "western_score.wav")
         self.left_paddle_color = WESTERN_1
         self.right_paddle_color = WESTERN_2
-        self.ball_image = "themes/ball_images/western_ball.png"
+        self.ball_image = join(CWD, "themes", "ball_images", "western_ball.png")
         self.set_vfx_volume(0.3, 0.3)
